27a9503ef03bc58acd8009b07d00ed612af6dad9
tarun-soni <soni.tarun26@gmail.com>
2021-09-13 20:58:21 +0530

    boilerplate and eslint

---
+++ App/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, SafeAreaView } from 'react-native'
+
+const App = () => {
+  return (
+    <SafeAreaView style={{ backgroundColor: 'red', width: 130 }}>
+      <Text>TEST</Text>
+    </SafeAreaView>
+  )
+}
+
+export default App
