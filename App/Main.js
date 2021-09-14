97c335b5fbd1d866e87fd489e0910d8cf7231122
tarun-soni <soni.tarun26@gmail.com>
2021-09-14 13:03:40 +0530

    added async storage for conditional onboarding

--- App/Main.js
+++ App/Main.js
@@ -1,21 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { Alert } from 'react-native'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 import OnboardingScreen from './screens/OnboardingScreen'
 import LoginScreen from './screens/LoginScreen'
 
 const MainStack = createNativeStackNavigator()
 
 const Main = () => {
-  return (
-    <MainStack.Navigator>
-      <MainStack.Screen
-        name="Onboarding"
-        component={OnboardingScreen}
-        options={{ headerShown: false }}
-      />
-      <MainStack.Screen name="Login" component={LoginScreen} />
-    </MainStack.Navigator>
-  )
+  const [isFirstLaunch, setIsFirstLaunch] = useState()
+
+  useEffect(() => {
+    const getData = async () => {
+      // await AsyncStorage.clear()
+      try {
+        const value = await AsyncStorage.getItem('@isAlreadyLaunched')
+        if (value !== null) setIsFirstLaunch(false)
+        else {
+          setIsFirstLaunch(true)
+          await AsyncStorage.setItem('@isAlreadyLaunched', 'true')
+        }
+      } catch (e) {
+        console.log(`error`, e)
+      }
+    }
+    getData()
+  }, [])
+
+  if (isFirstLaunch === null) {
+    Alert.alert('Please Restart the app')
+  }
+
+  if (isFirstLaunch === true) {
+    return (
+      <MainStack.Navigator>
+        <MainStack.Screen
+          name="Onboarding"
+          component={OnboardingScreen}
+          options={{ headerShown: false }}
+        />
+        <MainStack.Screen name="Login" component={LoginScreen} />
+      </MainStack.Navigator>
+    )
+    // eslint-disable-next-line no-else-return
+  } else {
+    return <LoginScreen />
+  }
 }
 
 export default Main
--- App/screens/LoginScreen.js
+++ App/screens/LoginScreen.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { SafeAreaView, Text } from 'react-native'
 
 const LoginScreen = () => {
   return (
-    <View>
-      <Text>LOGIN</Text>
-    </View>
+    <SafeAreaView style={{ backgroundColor: '#323232', flex: 1 }}>
+      <Text>test</Text>
+    </SafeAreaView>
   )
 }
 
